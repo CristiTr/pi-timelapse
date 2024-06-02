1c68afb021e97c8e360ae7e7c921b4cb98166b76
Cristian Tranulea <cristian.tranulea@gmail.com>
2024-06-02 21:37:51 +0300

    Refactor  for clarity and consistency

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const child_process = require("child_process");
 const INTERVAL = process.env.INTERVAL || 10000;
 const TIME_IN_H = process.env.TIME_IN_H || 1;
-const INTERATIONS = (TIME_IN_H * 60 * 60 * 1000) / INTERVAL;
+const ITERATIONS = (TIME_IN_H * 60 * 60 * 1000) / INTERVAL;
 const START_COUNT_FROM = process.env.START_COUNT_FROM || 1;
 
 let i = 1;
@@ -11,45 +11,45 @@ const t = new Date();
 console.log("\n\n " + t);
 console.log("Hours:" + TIME_IN_H);
 console.log("Interval: " + INTERVAL);
-console.log("Iterations: " + INTERATIONS);
+console.log("Iterations: " + ITERATIONS);
 console.log("Start file nr from: " + START_COUNT_FROM);
 console.log("Start taking photos");
 
 const interval = setInterval(() => {
   //check exit condition
-  if (i > INTERATIONS) {
+  if (i > ITERATIONS) {
     clearInterval(interval);
     console.log("Stopped taking photos");
-  } else {
-    console.log(i + " of " + INTERATIONS);
-    if (!takingPhoto) {
-      let fileNr = i + START_COUNT_FROM;
-      //take a photo
-      takingPhoto = true;
-      let filename = "data/image_" + fileNr + ".jpg";
-      let args = [
-        "-w",
-        "1280",
-        "-h",
-        "720",
-        "-q",
-        "70",
-        "-o",
-        filename,
-        "-t",
-        "1",
-      ];
-      // raspistill -w 1280 -h 720 -q 70 -o test.jpg
-      let spawn = child_process.spawn("raspistill", args);
+    return;
+  }
+  console.log(i + " of " + ITERATIONS);
+  if (!takingPhoto) {
+    let fileNr = i + parseInt(START_COUNT_FROM, 10);
+    //take a photo
+    takingPhoto = true;
+    let filename = "data/image_" + fileNr + ".jpg";
+    let args = [
+      "-w",
+      "1280",
+      "-h",
+      "720",
+      "-q",
+      "70",
+      "-o",
+      filename,
+      "-t",
+      "1",
+    ];
+    // raspistill -w 1280 -h 720 -q 70 -o test.jpg
+    let spawn = child_process.spawn("raspistill", args);
 
-      //photo is taken
-      spawn.on("exit", (code) => {
-        console.log(
-          "A photo is saved as " + filename + " with exit code, " + code
-        );
-        takingPhoto = false;
-      });
-    }
+    //photo is taken
+    spawn.on("exit", (code) => {
+      console.log(
+        "A photo is saved as " + filename + " with exit code, " + code
+      );
+      takingPhoto = false;
+    });
   }
   i++;
 }, INTERVAL);
